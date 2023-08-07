b7f8c864a1fd11a764242b9abe0289e861954211
AdebayoBunmiSolomon <solomon.bunmi@yahoo.com>
2023-08-07 17:17:59 +0100

    Deleting todo still in progress

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -16,21 +16,39 @@ import {
   GestureHandlerRootView,
   Swipeable,
 } from "react-native-gesture-handler";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const TasksList = ({ Tasks, Length }) => {
   const [selected, setIsSelected] = useState(false);
   const [todoTitle, setIsTodoTitle] = useState("");
   const [itemColor, setItemColor] = useState("");
-  const [itemIndex, setItemIndex] = useState();
 
   const todoData = Tasks;
   const todoLength = Length;
 
-  const deleteItem = () => {
-    const todoItem = todoData;
-    todoItem.splice(itemIndex, 1);
+  const deleteItem = async (selectedTodoTitle) => {
+    const getTodo = await AsyncStorage.getItem("todo");
+    const getTodoData = JSON.parse(getTodo);
+    const getTodoDataFormat = getTodoData;
+    const todoTitle = getTodoDataFormat.map((item) => item.title);
+    let i;
+    let copyOfGottenData;
+    let indexOfTodoTitle;
+    for (i = 0; i < todoTitle.length; i++) {
+      if (todoTitle[i] === selectedTodoTitle) {
+        indexOfTodoTitle = getTodoDataFormat.indexOf(getTodoData[i]);
+        console.log(indexOfTodoTitle);
+        // copyOfGottenData = getTodoDataFormat.slice(todoTitle[i]);
+        return i;
+      }
+
+      console.log("spliced data is", copyOfGottenData);
+    }
+    // const todoItem = todoData;
+    // todoItem.splice(itemIndex, 1);
     // console.log(newTodo);
     //setItemIndex("");
+    //console.log(selectedTodoTitle);
   };
 
   const truncateText = (str) => {
@@ -45,11 +63,10 @@ const TasksList = ({ Tasks, Length }) => {
     });
     return (
       <TouchableOpacity onPress={deleteItem}>
-        <View className="justify-center items-center bg-red-400 w-[100px] mt-3 h-20 rounded-lg">
+        <View className='justify-center items-center bg-red-400 w-[100px] mt-3 h-20 rounded-lg'>
           <Animated.Text
-            className="text-white text-sm font-bold"
-            style={{ transform: [{ scale: scale }] }}
-          >
+            className='text-white text-sm font-bold'
+            style={{ transform: [{ scale: scale }] }}>
             Delete
           </Animated.Text>
         </View>
@@ -59,46 +76,44 @@ const TasksList = ({ Tasks, Length }) => {
 
   return (
     <View>
-      <View className="ml-2 ">
-        <Text className="font-bold text-xl text-black">
+      <View className='ml-2 '>
+        <Text className='font-bold text-xl text-black'>
           Total todo: {todoLength}
         </Text>
       </View>
-      <View className="h-[650px] w-[95vw] mt-2 ml-2">
+      <View className='h-[650px] w-[95vw] mt-2 ml-2'>
         <FlatList
           renderScrollComponent={false}
           data={todoData}
           keyExtractor={(item) => item.title}
           renderItem={({ item, index }) => (
             <GestureHandlerRootView>
-              <Swipeable renderLeftActions={leftSwipe}>
+              <Swipeable renderLeftActions={leftSwipe} on>
                 <TouchableOpacity
-                  className="flex flex-row pt-2 mt-3 h-20 rounded-xl"
+                  className='flex flex-row pt-2 mt-3 h-20 rounded-xl'
                   style={[{ backgroundColor: item.color }]}
                   onPress={(e) => {
                     e.preventDefault();
                     setIsTodoTitle(String(item.title));
                     setItemColor(String(item.color));
                     setIsSelected(true);
-                  }}
-                >
+                  }}>
                   <View>
                     <TouchableOpacity
-                      className="h-10 w-10 rounded-lg justify-center items-center bg-slate-50 ml-2 mt-3"
+                      className='h-10 w-10 rounded-lg justify-center items-center bg-slate-50 ml-2 mt-3'
                       onPress={() => {
-                        setItemIndex(index);
-                      }}
-                    >
+                        setIsTodoTitle(String(item.title));
+                        deleteItem(todoTitle);
+                      }}>
                       <Text style={[{ color: item.color }]}>
-                        <Icon name="notebook-minus" size={32} />
+                        <Icon name='notebook-minus' size={32} />
                       </Text>
                     </TouchableOpacity>
                   </View>
                   <View>
                     <Text
-                      className="pt-5 text-[20px] font-medium pl-2"
-                      style={[{ color: "white" }]}
-                    >
+                      className='pt-5 text-[20px] font-medium pl-2'
+                      style={[{ color: "white" }]}>
                       {truncateText(String(item.title))}
                     </Text>
                   </View>
@@ -112,9 +127,8 @@ const TasksList = ({ Tasks, Length }) => {
         <Modal
           visible={selected}
           onRequestClose={() => setIsSelected(!selected)}
-          animationType="fade"
-          transparent={true}
-        >
+          animationType='fade'
+          transparent={true}>
           <More
             moreText={todoTitle}
             hideModal={() => setIsSelected(!selected)}
